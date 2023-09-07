7f0e06e382dfe6ce10a79c4e8a4cc9eecb8019d7
Titouan Février <91752826+ButtePaillade91@users.noreply.github.com>
2023-09-07 16:26:22 +0200

    Traduction du fichier Curve_RTA.py

--- COPS/Curve_RTA.py
+++ COPS/Curve_RTA.py
@@ -13,30 +13,30 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 # Date: 17 07 2023
-Comment = " On peut écrire ici un commenT_aire, qui seras imprimé dans le fichier txt"
+Comment = "We can write a comment which will be printed into the txt file"
 #Mat_Stack = write_stack_period(["Al_Rakic"], ["SiO2_I", "TiO2_I"], 4)
 Mat_Stack = ("BK7", "TiO2_I", "ZnO-Al_Rakic", "Ag_Babar", "ZnO-Al_Rakic", "TiO2_I", "SiO2_I")
-# Epaisseur des couche mince en nm 
+# Thin layers thickness in nm 
 d_Stack = [1000000, 30, 5, 8, 5, 22, 67]
-# volume fraction : loi de mélange (EMA)
+# volume fraction : mixing law
 vf = [0, 0, 0.7, 0, 0.5, 0, 0]
-# Domaine des longueurs d'ondes. Ici de 280 à 2500 nm par pas de 5 nm  
-Wl = np.arange(280,2505,5) # /!\. La valeur extréme supérieur est exclus
-# Angle d'incidence du rayonnement sur le stack
-Ang = 0 # en °
-# Ouverture et traitement des matériaux
+# Wavelengths domain. From 280 to 2500 nm with a 5 nm interval 
+Wl = np.arange(280,2505,5) # /!\. The extreme upper value is excluded
+# Incidence angle of the radiation on the stack
+Ang = 0 # in °
+# Materials opening and treatment
 n_Stack, k_Stack = Made_Stack(Mat_Stack, Wl)
 n_Stack, k_Stack = Made_Stack_vf(n_Stack, k_Stack, vf)
-# Ouverture du spectre solaire
+# Solar spectrum opening
 Wl_Sol , Sol_Spec , name_Sol_Spec= open_SolSpec('Materials/SolSpec.txt')
 Sol_Spec = np.interp(Wl, Wl_Sol, Sol_Spec)
 
 """_________________________________________________________________________"""
-# parameters est un dictionnaire qui contient les variables du problèmes
-# On donne le dictionnaire parameters comme entrée dans certaines fonctions
-# => Elles vont ensuite chercher les variables nécessaires   
+# parameters is a dictionary which includes the problem variables
+# We set parameters dictionary as an input for some functions
+# => Then, they go chekc necesary variables   
 
-parameters = {'Wl': Wl, # Je stocke une variable nommée "Wl", et lui donne la valeur de Wl
+parameters = {'Wl': Wl, # I store a variable named "Wl", and give it Wl value
             'Ang': Ang, 
             'Th_Substrate' : d_Stack[0],
             'Mat_Stack' : Mat_Stack,
@@ -44,35 +44,35 @@ parameters = {'Wl': Wl, # Je stocke une variable nommée "Wl", et lui donne la v
             'n_Stack' : n_Stack,
             'k_Stack' : k_Stack,}
 
-# /!\ Dans Python, il faut créer des listes vides avant de rajouter des choses dedans 
+# /!\ In Python, we must create empty lists before adding something in 
 
-print("Début du programme")
+print("Start of the program")
 launch_time = datetime.now().strftime("%Hh-%Mm-%Ss")
-print("Lancement à " + launch_time)
+print("Launch at " + launch_time)
 
 date_time = datetime.now().strftime("%Y-%m-%d-%Hh%M")
 directory = date_time
 if not os.path.exists(directory):
     os.makedirs(directory)
-print("Le dossier '" + directory + "' a été créé.")
+print("The '" + directory + "' directory has been created")
 
-"""___________________Données des meilleurs résultats______________________"""
+"""___________________Best results datas______________________"""
 
 t1 = time.time()
-# Calcul Rs, Ts, As du max
+# Calculate Rs, Ts, As of the maximum
 Rs, Ts, As = evaluate_RTA_s(d_Stack, parameters) 
-# Calcul le R, T, A
+# Calculate the R, T, A
 R, T, A = RTA_curve(d_Stack, parameters)
 
-# Ecriture du spectre solaire modifié
+# Writing of the modified solar spectrum
 Sol_Spec_mod = R*Sol_Spec
 t2 = time.time()
 
-"""________________________Ecriture des graphiquyes_________________________"""
+"""________________________Graphs writing_________________________"""
 
-# Graph de réflectance 
+# Reflectance graph 
 fig, ax1 = plt.subplots()
-color = 'black' # Couleurs de base possibles: b g r c m y k w
+color = 'black' # Basic colors available: b g r c m y k w
 ax1.set_xlabel('Wavelength (nm)')
 ax1.set_ylabel('Reflectivity (-)', color=color)
 ax1.plot(Wl, R, color=color)
@@ -83,15 +83,15 @@ ax2.set_ylabel('Solar Spectrum (W/m²nm⁻¹)', color=color)
 ax2.plot(Wl, Sol_Spec, color=color)
 ax2.tick_params(axis='y', labelcolor=color)
 fig.tight_layout()  
-ax2.set_ylim(0, 2) # changer l'échelle de l'axe y
+ax2.set_ylim(0, 2) # Change y-axis scale
 plt.title("Optimum reflectivity")
-# Sauvegarde de la figure
+# Figure save
 plt.savefig(directory + "/" + "Reflectance.png", dpi = 300, bbox_inches='tight')
 plt.show()
 
-# Graph de la transmittance
+# Transmitance graph
 fig, ax1 = plt.subplots()
-color = 'black' # Couleurs de base possibles: b g r c m y k w
+color = 'black' # Basic colors available: b g r c m y k w
 ax1.set_xlabel('Wavelength (nm)')
 ax1.set_ylabel('Transmissivity (-)', color=color)
 ax1.plot(Wl, T, color=color)
@@ -102,12 +102,12 @@ ax2.set_ylabel('Solar Spectrum (W/m²nm⁻¹)', color=color)
 ax2.plot(Wl, Sol_Spec, color=color)
 ax2.tick_params(axis='y', labelcolor=color)
 fig.tight_layout()  
-ax2.set_ylim(0, 2) # changer l'échelle de l'axe y
+ax2.set_ylim(0, 2) # Change y-axis scale
 plt.title("Optimum transmissivity")
 plt.savefig(directory + "/" + "Transmittance.png", dpi = 300, bbox_inches='tight')
 plt.show()
 
-"""_____________________Ecriture des résultats dans des txt_________________"""
+"""_____________________Results writing in txt files_________________"""
 
 filename = directory + "/performance.txt"
 with open(filename, "w") as file:
@@ -123,31 +123,31 @@ filename = directory + "/StacksThicknesses.txt"
 with open(filename, "w") as file:
     file.write(str(d_Stack) + "\n")
         
-print("Les résultats ont été écrits dans le dossier")
+print("Results have been successfully writed in the directory")
 
 filename = directory + "/RTA.txt"
 with open(filename, "w") as file:
     for i in range(len(A)):
         file.write(str(Wl[i]) + "\t" + str(R[i]) + "\t" + str(T[i]) + "\t" + str(A[i]) + "\n")
         
-print("Les données RTA du meilleur empillement ont été écrites dans cet ordre")
+print("RTA datas of the best stack have been writed in this order")
 
 filename = directory + "/simulation.txt"
 script_name = os.path.basename(__file__)
 with open(filename, "w") as file:
-    file.write("Le nom du fichier est : " + str(script_name) + "\n")
-    file.write("Heure de lancement " + str(launch_time) + "\n")
+    file.write("File name is : " + str(script_name) + "\n")
+    file.write("Launch time is " + str(launch_time) + "\n")
     file.write(str(Comment) + "\n")
     file.write("_____________________________________________" +  "\n")
-    file.write("Matériaux de l'empillement\t" + str(Mat_Stack) + "\n")
-    file.write("Domaine des longueurs d'ondes \t" + str(min(Wl)) + " nm à " + str(max(Wl)) + " nm, pas de " + str(Wl[1]-Wl[0])+ " nm"+ "\n")
-    file.write("Epaisseur du substrat\t" + str(d_Stack[0]) + "nm" +  "\n")
-    file.write("Epaisseur des couches minces\t" + str(d_Stack[1:]) + " nm" + "\n")
-    file.write("Angle d'incidence sur le stack\t" + str(Ang) + "°" + "\n")
+    file.write("Stack materials\t" + str(Mat_Stack) + "\n")
+    file.write("Wavelengths domain\t" + str(min(Wl)) + " nm at " + str(max(Wl)) + " nm, interval of " + str(Wl[1]-Wl[0])+ " nm"+ "\n")
+    file.write("Substrate thickness\t" + str(d_Stack[0]) + "nm" +  "\n")
+    file.write("Thin layers thickness\t" + str(d_Stack[1:]) + " nm" + "\n")
+    file.write("Incidence angle on the stack\t" + str(Ang) + "°" + "\n")
 
-print("Les noms et valeurs des variables de la simulation ont été écrites")
+print("Variables names and values pf the simulation have been writed")
 
-# J'écrit les indices des matériaux dans un fichier texte
+# I write materials indexes in a text file
 result = eliminate_duplicates(Mat_Stack)
 n_Stack_w, k_Stack_w = Made_Stack(result[0], Wl)
 
@@ -155,19 +155,19 @@ for i in range(len(result[0])):
     name = result[0][i]
     n = n_Stack_w[:,i]
     k = k_Stack_w[:,i]
-    # Trace le graphique
+    # plot the graph
     plt.title("N et k de" + name)
     plt.plot(Wl, n, label = "n extrapolé")
     plt.plot(Wl, k, label = "k extrapolé")
 # =============================================================================
-#     # Ouvre le fichier originel
+#     # Open the initial file
 #     Wl_2, n_2, k_2 = open_material(result[0][i])
 #     plt.plot(Wl_2, n_2, 'o', label = "n data")
 #     plt.plot(Wl_2, k_2, 'o', label = "k data")
 # =============================================================================
-    plt.xlabel("Longueur d'onde (nm)")
+    plt.xlabel("Wavelength (nm)")
     plt.legend()
-    plt.ylabel("Valeur de n et k (-)")
+    plt.ylabel("n and k values (-)")
     plt.savefig(directory + "/" + "refractive_index"+ name + ".png", dpi = 300, bbox_inches='tight')
     plt.close()
     #filename = "/" + str(name) + ".txt"
@@ -176,7 +176,7 @@ for i in range(len(result[0])):
         for i in range(len(n)):
             file.write(str(Wl[i]) + "\t" + str(n[i]) + "\t" + str(k[i])+ "\n")
             
-print("Les n,k de chaque matériaux ont été écrits dans un fichier à leurs noms")
+print("The n,k of each material have been writd in a file named as them")
 
 
 
