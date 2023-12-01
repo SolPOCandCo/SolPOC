1dabe9ad3ae3f17f2accd3ac4426685a0ffa2e08
Thalita Drumond <thalitafdrumond@gmail.com>
2023-12-01 01:00:20 +0100

    new strategy tested and implemented seed list needed to be created before launching the pool. Previous method was not guaranteeing the same results

---
+++ tests/fixture_data.py
@@ -0,0 +1,82 @@
+import numpy as np
+from pytest import fixture
+import solpoc
+
+
+def get_base_parameters():
+    parameters = {'seed': 3245298}
+    # Wavelength domain, here from 320 to 2500 nm with a 5 nm step. Can be change!
+    Wl = np.arange(280, 2505, 5)  # /!\ Last value is not included in the array
+# Thickness of the substrate, in nm
+    # Open the solar spectrum
+    Wl_Sol, Sol_Spec, name_Sol_Spec = solpoc.open_SolSpec(
+        'Materials/SolSpec.txt', 'GT')
+    # Open a file with PV cell shape
+    Wl_PV, Signal_PV, name_PV = solpoc.open_Spec_Signal(
+        'Materials/PV_cells.txt', 1)
+    # Open a file with thermal absorber shape
+    Wl_Th, Signal_Th, name_Th = solpoc.open_Spec_Signal(
+        'Materials/Thermal_absorber.txt', 1)
+    # Open and interpol the refractive index
+    Mat_Stack = ["BK7", "TiO2", "SiO2"]
+    n_Stack, k_Stack = solpoc.Made_Stack(Mat_Stack, Wl)
+    # Open and processing the reflectif index of materials used in the stack (Read the texte files in Materials/ )
+    # Interpolate the solar spectrum
+    Sol_Spec = np.interp(Wl, Wl_Sol, Sol_Spec)
+    Signal_PV = np.interp(Wl, Wl_PV, Signal_PV)  # Interpolate the signal
+    Signal_Th = np.interp(Wl, Wl_Th, Signal_Th)  # Interpolate the signal
+    parameters.update({
+        'Wl':  Wl,  # Thickness of the substrate, in nm
+        'Ang': 0,  # Incidence angle on the thin layers stack, in °
+        'C': 80,  # Solar concentration. Data necessary for solar thermal application, like selective stack
+        'T_abs': 300 + 273,  # Thermal absorber temperature, in Kelvin. Data necessary for solar thermal application, like selective stack
+        # Air temperature, in Kelvin. Data necessary for solar thermal application, like selective stack
+        'T_air': 20 + 273,
+        'Sol_Spec': Sol_Spec,
+        'name_Sol_Spec': name_Sol_Spec,
+        'Th_range': (0, 200),  # in nm.,
+        'Th_Substrate':  1e6,  # Substrate thickness, in nm
+        'Signal_PV': Signal_PV,
+        'Signal_Th': Signal_Th,
+        'Mat_Stack': Mat_Stack,
+        # Cuting Wavelenght. Data necessary for low-e, RTR or PV_CSP evaluates functions
+        'Lambda_cut_1': 800,  # nm
+        'Lambda_cut_2': 1000,  # nm
+        # Range of refractive index (lower bound and upper bound), for the optimisation process
+        'n_range': (1.3, 3.0),
+        'n_Stack': n_Stack,
+        'k_Stack': k_Stack,
+    })
+
+    return parameters
+
+
+def get_DEvol_parameters():
+    parameters = {}
+    # Hyperparameters for optimisation methods
+    # Choice of optimisation method
+    algo = solpoc.DEvol  # Name of the optimization method
+    # Callable. Name of the selection method : selection_max or selection_min
+    selection = solpoc.selection_max
+    evaluate = solpoc.evaluate_T_s  # Callable. Name of the cost function
+    parameters.update({
+        'pop_size': 30,  # number of individual per iteration / generation ,
+        'algo': algo,  # Name of the optimization method ,
+        'name_algo': algo.__name__,
+        'evaluate': evaluate,  # Callable. Name of the cost function,
+        # Callable. Name of the selection method : selection_max or selection_min,
+        'selection': selection,
+        'name_selection': selection.__name__,
+        # chance of child gene muted during the birth. /!\ This is Cr for DEvol optimization method
+        'mutation_rate': 0.5,
+        # If a chromosome mutates, the value change from random number include between + or - this values
+        'mutation_delta': 15,
+        # Number of generation/iteration. For DEvol is also used to calculate the budget (nb_generation * pop_size)
+        'nb_generation': 4,
+        'mutation_DE': "current_to_best",
+        'f1': 0.9, 'f2': 0.8,  # Hyperparameter for DEvol
+    })
+    return parameters
+
+
+
---
+++ tests/mp_test.py
@@ -0,0 +1,71 @@
+# test for multiprocessing
+
+import solpoc
+import numpy as np
+from fixture_data import get_DEvol_parameters, get_base_parameters
+
+from multiprocessing import Pool
+
+# elements that go through the multiproc pool need to be declared at top level
+mp_parameters = {}
+mp_parameters.update(get_base_parameters())
+mp_parameters.update(get_DEvol_parameters())
+nb_run=10
+
+# Test 1: test seed control of the parallel runs
+mp_parameters['seed_list'] = solpoc.get_seed_from_randint(
+    nb_run, 
+    rng=np.random.RandomState(mp_parameters['seed']))
+
+
+def single_run(i):
+    this_run_params = {}
+    this_run_params.update(mp_parameters)
+    this_run_params['seed'] = mp_parameters['seed_list'][i]
+    return this_run_params['seed']
+
+
+def test_multiprocessesing_seeds():
+    global mp_parameters, nb_run
+
+    # ensure same list is generated
+    initial_rng = np.random.RandomState(mp_parameters['seed'])
+    seed_list_initial = solpoc.get_seed_from_randint(nb_run, initial_rng)
+
+    assert np.all(np.equal(seed_list_initial, mp_parameters['seed_list']))
+
+    # ensure seeds reach the different processes
+    # we make them return the seed they got, 
+    # then check if we get the same seeds in the same order
+    pool = Pool(2)
+    seed_list_after_map = pool.map(single_run, range(nb_run))
+    assert np.all(np.equal(
+        seed_list_initial, seed_list_after_map))
+
+# Test 2: test launching one optim algo in parallel with the controled seeds
+# ensure results are reproduced
+def one_DEvol_run(i):
+    this_run_params = {}
+    this_run_params.update(mp_parameters)
+    this_run_params['seed'] = mp_parameters['seed_list'][i]
+
+    best_solution, dev, n_iter, seed = this_run_params['algo'](
+        this_run_params['evaluate'], 
+        this_run_params['selection'], 
+        this_run_params)
+
+    best_solution = np.array(best_solution)
+
+    return best_solution
+
+def test_multiprocessesing_reprod_results():
+    # ensure same solution is found when executing pool map on the same inputs
+    global mp_parameters, nb_run
+
+    pool = Pool(2)
+    solution1 = pool.map(one_DEvol_run, range(nb_run))
+    solution2 = pool.map(one_DEvol_run, range(nb_run))
+
+    assert np.all(np.equal(
+        solution1, solution2))
+
