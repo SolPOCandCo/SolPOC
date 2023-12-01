39ccae87b77fcbf3b48d8fc36b1c696183e70b7c
Thalita Drumond <thalitafdrumond@gmail.com>
2023-12-01 10:05:26 +0100

    reformat (autopep8)

--- tests/functions_SolPOC_test.py
+++ tests/functions_SolPOC_test.py
@@ -4,24 +4,27 @@ from pytest import fixture
 
 from fixture_data import get_DEvol_parameters, get_base_parameters
 
+
 @fixture()
 def base_parameters():
     return get_base_parameters()
+
 
 @fixture
 def DEvol_parameters():
     return get_DEvol_parameters()
 
 
-
 def test_RTA3C():
     # Test case taken from function docstring
 
     # Write these variables :
-    l = np.arange(600,750,100) # We can notice that two wavelengths are calculated : 600 and 700 nm.
-    d = np.array([[1000000, 150, 180]])  # 1 mm of substrate, 150 nm of n°1 layer, 180 of n°2 and empty space (n=1, k=0).
-    n = np.array([[1.5, 1.23,1.14],[1.5, 1.2,1.1]])
-    k = np.array([[0, 0.1,0.05], [0, 0.1, 0.05]])
+    # We can notice that two wavelengths are calculated : 600 and 700 nm.
+    l = np.arange(600, 750, 100)
+    # 1 mm of substrate, 150 nm of n°1 layer, 180 of n°2 and empty space (n=1, k=0).
+    d = np.array([[1000000, 150, 180]])
+    n = np.array([[1.5, 1.23, 1.14], [1.5, 1.2, 1.1]])
+    k = np.array([[0, 0.1, 0.05], [0, 0.1, 0.05]])
     Ang = 0
 
     Refl, Trans, Abs = solpoc.RTA3C(l, d, n, k, Ang)
@@ -55,28 +58,18 @@ def test_get_seed_for_run():
     assert np.all(np.equal(out_call1, out_call2))
 
 
-
-
-
-
-
-
-
 def test_DEvol(base_parameters, DEvol_parameters):
     # only tests if it is running
     parameters = base_parameters
     parameters.update(DEvol_parameters)
 
     best_solution, dev, n_iter, seed = parameters['algo'](
-        parameters['evaluate'], 
-        parameters['selection'], 
+        parameters['evaluate'],
+        parameters['selection'],
         parameters)
 
     best_solution = np.array(best_solution)
     perf = parameters['evaluate'](best_solution, parameters)
-
-
-
 
 
 # TODO complete with tests for other functions in file
--- tests/mp_test.py
+++ tests/mp_test.py
@@ -10,11 +10,11 @@ from multiprocessing import Pool
 mp_parameters = {}
 mp_parameters.update(get_base_parameters())
 mp_parameters.update(get_DEvol_parameters())
-nb_run=10
+nb_run = 10
 
 # Test 1: test seed control of the parallel runs
 mp_parameters['seed_list'] = solpoc.get_seed_from_randint(
-    nb_run, 
+    nb_run,
     rng=np.random.RandomState(mp_parameters['seed']))
 
 
@@ -35,7 +35,7 @@ def test_multiprocessesing_seeds():
     assert np.all(np.equal(seed_list_initial, mp_parameters['seed_list']))
 
     # ensure seeds reach the different processes
-    # we make them return the seed they got, 
+    # we make them return the seed they got,
     # then check if we get the same seeds in the same order
     pool = Pool(2)
     seed_list_after_map = pool.map(single_run, range(nb_run))
@@ -44,19 +44,22 @@ def test_multiprocessesing_seeds():
 
 # Test 2: test launching one optim algo in parallel with the controled seeds
 # ensure results are reproduced
+
+
 def one_DEvol_run(i):
     this_run_params = {}
     this_run_params.update(mp_parameters)
     this_run_params['seed'] = mp_parameters['seed_list'][i]
 
     best_solution, dev, n_iter, seed = this_run_params['algo'](
-        this_run_params['evaluate'], 
-        this_run_params['selection'], 
+        this_run_params['evaluate'],
+        this_run_params['selection'],
         this_run_params)
 
     best_solution = np.array(best_solution)
 
     return best_solution
+
 
 def test_multiprocessesing_reprod_results():
     # ensure same solution is found when executing pool map on the same inputs
@@ -68,4 +71,3 @@ def test_multiprocessesing_reprod_results():
 
     assert np.all(np.equal(
         solution1, solution2))
-
