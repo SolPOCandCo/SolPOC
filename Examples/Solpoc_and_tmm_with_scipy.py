79624dca83138cbc62d946d459407aab11471011
Antoine Grosjean <mr.grosjean.antoine@gmail.com>
2025-09-17 16:12:57 +0200

    Deplace des fichiers
    je déplace des fichiers

---
+++ Examples/Solpoc_and_tmm_with_scipy.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 13 10:41:51 2025
+@author: agrosjean
+"""
+
+import numpy as np
+from scipy.optimize import minimize
+from tmm_fast.vectorized_tmm_dispersive_multistack import coh_vec_tmm_disp_mstack as tmm
+import solpoc as sol
+import time 
+
+
+# Objective function for the anti-reflection coating
+def AR_coating_tmm_nm(d_list, M, theta, wl, Sol_Spec):
+    """
+    Objective function for the anti-reflection coating using TMM (thicknesses in nanometers)
+    This version expects layer thicknesses in nanometers for better numerical stability.
+    The substrate and air are treated as semi-infinite layers.
+    
+    Parameters:
+       d_list: 1D array of thin layer thicknesses to optimize (excluding substrate and air), in nm ! 
+       M: refractive index matrix (num_samples, num_layers, num_wavelengths)
+       theta: incidence angles (radians)
+       wl: wavelengths (meters)
+       Sol_Spec: solar spectrum interpolated on wl
+    """
+
+    T = np.zeros((1, 5))
+
+    # Assign optimized thicknesses
+    T[:, 1:-1] = d_list *1e-9 # assuming d_list shape = (num_layers-2,)
+
+    # Substrate and air are semi-infinite
+    T[:, 0] = 1e6
+    T[:, -1] = np.inf
+    
+    # Compute TMM for s- and p-polarizations
+    O_s = tmm('s', M, T, theta, wl, device='cpu')
+    O_p = tmm('p', M, T, theta, wl, device='cpu')
+
+    # Average reflectivity
+    R_s = O_s['R'][0, 0, :]
+    R_p = O_p['R'][0, 0, :]
+    R = (R_s + R_p) / 2
+
+    # Compute solar reflectivity
+    
+    Rs = sol.SolarProperties(wl, R, Sol_Spec)
+  
+    return Rs
+
+# Objective function for the anti-reflection coating
+def AR_coating_tmm(d_list, M, theta, wl, Sol_Spec):
+    """
+    Objective function for the anti-reflection coating using TMM package
+    This version expects layer thicknesses in meters (no unit conversion), as expected by tmm.
+    The function constructs the thickness matrix T for TMM, assigning:
+       - Optimized thicknesses to the thin layers (d_list)
+       - Semi-infinite values for the substrate (T[:, 0]) and air (T[:, -1])
+    It then computes the reflectivity for both s- and p-polarizations,
+    averages them, and calculates the solar reflectivity by weighting
+    the spectrum with the solar irradiance (Sol_Spec).
+    # 
+    Parameters:
+       d_list: 1D array of thin layer thicknesses to optimize (excluding substrate and air)
+       M: refractive index matrix (num_samples, num_layers, num_wavelengths)
+       theta: incidence angles (radians)
+       wl: wavelengths (meters)
+       Sol_Spec: solar spectrum interpolated on wl
+    """
+
+    T = np.zeros((1, 5))
+
+    # Assign optimized thicknesses
+    T[:, 1:-1] = d_list  # assuming d_list shape = (num_layers-2,)
+
+    # Substrate and air are semi-infinite
+    T[:, 0] = 1e6
+    T[:, -1] = np.inf
+    
+    # Compute TMM for s- and p-polarizations
+    O_s = tmm('s', M, T, theta, wl, device='cpu')
+    O_p = tmm('p', M, T, theta, wl, device='cpu')
+
+    # Average reflectivity
+    R_s = O_s['R'][0, 0, :]
+    R_p = O_p['R'][0, 0, :]
+    R = (R_s + R_p) / 2
+
+    # Compute solar reflectivity
+    
+    Rs = sol.SolarProperties(wl, R, Sol_Spec)
+  
+    return Rs
+
+# Objective function for the anti-reflection coating
+def AR_coating_solpoc(d_Stack, n_Stack, k_Stack, Wl, Ang, Sol_Spec):
+    # Convert d_Stack (received as ndarray) to list
+    d_list = list(d_Stack)
+
+    # Add the substrate thickness (BK7, 1e6 nm) at the beginning
+    d_full = [1e6] + d_list
+
+    # Make it a 2D array (shape (1, n_layers))
+    d_Stack = np.array([d_full])
+    
+    # Calculate spectral reflectivity, transmissivity, and absorptivity
+    # over all wavelengths for a single incidence angle
+    R, T, A = sol.RTA(Wl, d_Stack, n_Stack, k_Stack, Ang)
+    # Calculate the solar reflectivity
+    Rs = sol.SolarProperties(Wl, R, Sol_Spec)
+  
+    return Rs
+
+# Wavelength domain, in nanometers: from 400 to 800 nm with 5 nm step
+Wl = np.arange(280, 2505, 5)
+
+# Describe the thin layer stack.
+# In SolPOC, it is not necessary to add air (n=1) above the stack
+Mat_Stack = ["n15", "n17", "n147", "n137"]
+
+# Incidence angle of light in the stack, in degrees
+Ang = 0  
+
+# Get refractive index (n) and extinction coefficient (k)
+n_Stack, k_Stack = sol.Made_Stack(Mat_Stack, Wl)
+
+Wl_Sol, Sol_Spec, name_Sol_Spec = sol.open_SolSpec('Materials/SolSpec.txt', 'GT')
+Sol_Spec = np.interp(Wl, Wl_Sol, Sol_Spec)
+
+np.random.seed(512)
+# Starting point for the layer thicknesses to optimize (in nm)
+x_AR = np.random.uniform(low=0, high=300, size=3)
+
+t1 = time.time()
+# Run the optimization
+result_AR = minimize(
+    AR_coating_solpoc, 
+    x_AR, 
+    args=(n_Stack, k_Stack, Wl, Ang, Sol_Spec), 
+    method= 'BFGS'
+)
+t2 = time.time()
+print("SolPOC basic solar antireflectif coating : ")
+print("Time calcul", str(t2-t1), "seconde")
+print("Success:", result_AR.success)
+print("Optimized thicknesses [nm]:", result_AR.x)
+print("Average solar reflectivity:", result_AR.fun)
+
+# ------------------------ Parameters ------------------------
+wl = np.arange(280, 2505, 5) * 1e-9
+theta = np.array([0]) * (np.pi/180)        # incidence angle in radians
+num_layers = 5                              # includes substrate and air
+
+# Refractive indices for each layer
+M = np.ones((1, num_layers, wl.shape[0]))
+M[:, 0, :] = 1.5  # substrate
+M[:, 1, :] = 1.7  # layer 1
+M[:, 2, :] = 1.47  # layer 2
+M[:, 3, :] = 1.37 # layer 3
+
+# Solar spectrum
+Wl_Sol, Sol_Spec_data, name_Sol_Spec = sol.open_SolSpec('Materials/SolSpec.txt', 'GT')
+Sol_Spec = np.interp(wl* 1e9, Wl_Sol, Sol_Spec_data)
+
+# ------------------------ Initial thicknesses ------------------------
+"""
+Depending of the method, It can be s better to keep the thicknesses in nanometers for the optimization.
+When using meters, the values are very small (~1e-7), which can cause
+numerical instability in gradient-based optimizers like BFGS.
+This is why SolPOC uses nanometers for layer thicknesses.
+"""
+max_t = 300  # 300 nm
+min_t = 0    # 0 nm
+
+# Only thin layers are optimized (exclude substrate and air)
+np.random.seed(512)
+x_AR = np.random.uniform(min_t, max_t, num_layers - 2)  
+
+# ------------------------ Optimization ------------------------
+t1 = time.time()
+result_AR = minimize(
+    AR_coating_tmm_nm, 
+    x_AR, 
+    args=(M, theta, wl, Sol_Spec), 
+    method='BFGS'
+)
+t2 = time.time()
+
+print("TMM basic solar anti-reflective coating:")
+print("Time calcul", str(t2-t1), "seconde")
+print("Success:", result_AR.success)
+print("Optimized thicknesses [m]:", result_AR.x)
+print("Average solar reflectivity:", result_AR.fun)
+
+# ------------------------ Initial thicknesses ------------------------
+max_t = 300*1e-9  # 300 nm
+min_t = 0 * 1e-9   # 0 nm
+
+# Only thin layers are optimized (exclude substrate and air)
+np.random.seed(512)
+x_AR = np.random.uniform(min_t, max_t, num_layers - 2)  
+# ------------------------ Optimization ------------------------
+t1 = time.time()
+result_AR = minimize(
+    AR_coating_tmm, 
+    x_AR, 
+    args=(M, theta, wl, Sol_Spec), 
+    method='BFGS'
+)
+t2 = time.time()
+
+print("TMM basic solar anti-reflective coating:")
+print("Time calcul", str(t2-t1), "seconde")
+print("Success:", result_AR.success)
+print("Optimized thicknesses [m]:", result_AR.x)
+print("Average solar reflectivity:", result_AR.fun)
---
+++ Examples/fit_experimental_signal.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2025-09-17
+SolPOC v 0.9.7
+@authors: A.Grosjean (main author, EPF, France), A.Soum-Glaude (PROMES-CNRS, France), A.Moreau (UGA, France) & P.Bennet (UGA, France)
+contact : antoine.grosjean@epf.fr
+"""
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import os
+import solpoc as sol
+from datetime import datetime
+from multiprocessing import Pool, cpu_count
+"""
+This script performs a fitting procedure to determine the thicknesses of thin-film layers in a 
+measured multilayer stack. The goal is to match the experimentally measured reflectance and transmittance 
+with the values calculated by the code. Using a adapted cost function (labeled with : _fit),
+the algorithm adjusts the layer thicknesses to minimize the difference (error) between the measured and 
+calculated optical responses.
+"""
+#----------------------------------------------------------------------------#
+#                   SCRIPT PARAMETERS - START                                #
+#----------------------------------------------------------------------------#
+# %%  Main : You can start to modified something
+# Comment to be written in the simulation text file
+Comment = "Test of fiting : found the thin layers thcinesses according to a reflectance and transmittance measurement" # Comment to be written in the simulation text file
+# Write the thin layer stack, to substrat to ambiant 
+Mat_Stack = ["BK7", "SiO2", "TiO2"]
+cost_function = sol.evaluate_fit_RT # Callable. Cost function for the fitting, here R + T are fit
+# %% Important parameters
+Th_Substrate = 1e6  # Substrate thickness, in nm
+Th_range = (0, 300) # Range of thickness (lower bound and upper bound), for the fitt process
+Ang = 0  # Incidence angle on the thin layers stack, in °
+# %% Signal to fit, in a "RTA" texte file. 
+"""
+/!\ Example: Here we load an RTA text file located in the "Fit" folder. The file contains experimental data. 
+# The first call reads the wavelength (nm) and the reflectance (0–1 range),
+# and the second call reads the wavelength (nm) and the transmittance (0–1 range).
+"""
+Wl_fit, Signal_fit, name_fit = sol.open_Spec_Signal('Fit/signal_fit.txt', 1) # Read the wavelenght (nm) and reflectance (-), in 0-1 range
+Wl_fit, Signal_fit_2, name_fit = sol.open_Spec_Signal('Fit/signal_fit.txt', 2) # Read the wavelenght (nm) and transmittance (-) in 0-1 range
+# %% Hyperparameters for optimization methods
+# Number used to calculate the budget : nb_generation * pop_size 
+budget = 2000 # 
+nb_run = 8  # Number of run
+cpu_used = 8  # Number of CPU used. /!\ be "raisonable", regarding the real number of CPU your computer
+seed = None # Seed of the random number generator. Uncomment for fix the seed
+#----------------------------------------------------------------------------#
+#                   SCRIPT PARAMETERS - END                                  #
+#----------------------------------------------------------------------------#
+# %% You should stop modifying anything :)
+"""_________________________________________________________________________"""
+"""
+Based on few experimented, DEvol (with "current to best" mutation strategies) is useful for "fitting" process. 
+Is here an optimization : we reduce the difference between a reflectance and transmittance with an 
+experimental reflectance and transmittance (in text file, placed in the Fit folder) 
+If cost function near 0 : the difference is few, and the model reflectance is near the real reflectance. 
+"""
+algo = sol.DEvol  # Name of the optimization method
+mutation_DE = "current_to_best"
+pop_size = 30  # number of individual per iteration / generation
+crossover_rate = 0.5  # crossover rate (1.0 = 100%)
+f1, f2 = 0.9, 0.8  # Hyperparameter for DEvol
+
+# String. Mutaton method for DEvol optimization method
+
+selection = sol.selection_min # Callable. Name of the selection method : here selection_min
+cost_function = sol.evaluate_fit_RT # Callable. Name of the cost function, here fit
+Wl_Sol, Sol_Spec, name_Sol_Spec = sol.open_SolSpec('Materials/SolSpec.txt', 'DC')# Open the solar spectrum
+Wl = Wl_fit
+Sol_Spec = np.interp(Wl, Wl_Sol, Sol_Spec)  # Interpolate the solar spectrum
+# Open and interpol the refractive index
+n_Stack, k_Stack = sol. Made_Stack(Mat_Stack, Wl)
+# parameters is a dictionary containing the problem variables
+# => They then read the necessary variables  with the commande .get
+
+parameters = sol.get_parameters(
+    Mat_Stack = Mat_Stack,
+    algo = algo,
+    cost_function = cost_function,
+    selection = selection,  
+    Wl = Wl,
+    Ang = Ang,
+    Sol_Spec = Sol_Spec,
+    name_Sol_Spec=name_Sol_Spec,
+    Signal_fit = Signal_fit,
+    Signal_fit_2 = Signal_fit_2,
+    n_Stack = n_Stack,
+    k_Stack = k_Stack,
+    Th_range = Th_range,
+    Th_Substrate = Th_Substrate,
+    pop_size=pop_size,
+    f1 = f1,
+    f2 = f2, 
+    mutation_DE = mutation_DE,
+    crossover_rate = crossover_rate,
+    budget = budget,
+    nb_run = nb_run,
+    seed  = seed,
+)
+
+# %%
+
+# %%
+# creation of a function for multiprocessing
+def run_problem_solution(i):
+    t1 = time.time()  # Time before the optimisation process
+    # Line below to be uncommented to slightly desynchronize the cores, if the seed is generated by reading the clock.
+    # time.sleep(np.random.random())
+    # Create a dictionary for this particular run so we can update the seed with the specific seed for this run
+    this_run_params = {}
+    this_run_params.update(parameters)
+    this_run_params['seed'] = parameters['seed_list'][i]
+    # Run the optimisation process (algo), with an evaluate method, a selection method and the parameters dictionary.
+    best_solution, dev, n_iter, seed = algo(
+        cost_function, selection, this_run_params)
+    # calculate the time used
+    t2 = time.time()
+    temps = t2 - t1
+    # best solution is a stack. Evaluation of this stack
+    if type(best_solution) != list:
+        best_solution = best_solution.tolist()
+    best_solution = np.array(best_solution)
+    dev = np.array(dev)
+    perf = cost_function(best_solution, parameters)
+    print("I finished case #", str(i+1), " in ", "{:.1f}".format(
+            temps), " seconds.", " Best: ", "{:.4f}".format(perf), flush=True)
+
+    return best_solution, perf, dev, n_iter, temps, seed
+
+# %%
+# Beginning of the main loop. The code must be in this loop to work in multiprocessing
+if __name__ == "__main__":
+
+    print("Start of the program")
+    launch_time = datetime.now().strftime("%Hh-%Mm-%Ss")
+    print("Launched at " + launch_time)
+    print("Number of detected cores: ", cpu_count())
+    print("Number of used cores: ", cpu_used)
+    date_time = datetime.now().strftime("%Y-%m-%d-%Hh%M")
+    dawn_of_time = time.time()
+    
+    sol.run_main(parameters)
+    directory = parameters.get('directory')
+    mp_pool = Pool(cpu_used)
+    # Solving each problem in the pool using multiprocessing
+    results = mp_pool.map(run_problem_solution, range(nb_run))
+
+    # Creation of empty lists, for use them later
+    tab_best_solution = []
+    tab_dev = []
+    tab_perf = []
+    tab_n_iter = []
+    tab_temps = []
+    tab_seed = []
+    # sleep 1 ms
+    time.sleep(1)
+    # result is an array containing the solutions returned by my.pool. I extract them and place them in different arrays
+    for i, (best_solution, perf, dev, n_iter, temps, seed) in enumerate(results):
+        # I add the values to the tables
+        tab_perf.append(perf)
+        tab_dev.append(dev)
+        tab_best_solution.append(best_solution)
+        tab_n_iter.append(n_iter)
+        tab_temps.append(temps)
+        tab_seed.append(seed)
+        
+    Experience_results = {'tab_perf': tab_perf,
+                          'tab_dev': tab_dev,
+                          'tab_best_solution': tab_best_solution,
+                          'tab_n_iter': tab_n_iter,
+                          'tab_temps': tab_temps,
+                          'tab_seed': tab_seed,
+                          'Comment': Comment,
+                          'name_PV': "none",
+                          'name_Th': "none",
+                          'name_Sol_Spec': name_Sol_Spec,
+                          'launch_time': launch_time,
+                          'cpu_used': cpu_used,
+                          'nb_run': nb_run, }  # End of the dict
+    end_of_time = time.time()
+    time_real = end_of_time - dawn_of_time
+    parameters.update({"time_real": time_real})
+    print("The total real time is: ",
+              "{:.2f}".format(time_real), "seconds")
+    print("The processor calculation real time is: ",
+              "{:.2f}".format(sum(tab_temps)), "seconds")
+
+    """_____________________Best results datas______________________"""
+
+    sol.Explain_results_fit(parameters, Experience_results)
+
+    """_____________________Write results in a text file_________________"""
+
+    sol.Convergences_txt(parameters, Experience_results, directory)
+    sol.Generate_txt(parameters, Experience_results, directory)
+
+    """________________________Plot creation_________________________"""
+    sol.Reflectivity_plot_fit(parameters, Experience_results, directory)
+    sol.Transmissivity_plot_fit(parameters, Experience_results, directory)
+    sol.Convergence_plots_2(parameters, Experience_results, directory)
+    sol.Consistency_curve_plot(parameters, Experience_results, directory)
+    sol.Optimum_thickness_plot(parameters, Experience_results, directory)
+    sol.Volumetric_parts_plot(parameters, Experience_results, directory)
+# %%
