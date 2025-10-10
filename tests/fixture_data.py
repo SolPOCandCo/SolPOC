41371f44ab2ec99bbd82d99cd7694fec0b579c51
Thalita Drumond <thalitafdrumond@gmail.com>
2025-10-10 15:30:38 +0200

    Moved tests outside solpoc folder + test fixes After updates to some of the core functions, some fixes on the tests were needed.

--- tests/fixture_data.py
+++ tests/fixture_data.py
@@ -3,7 +3,6 @@ import solpoc
 
 
 def get_base_parameters():
-    parameters = {'seed': 3245298}
     # Wavelength domain, here from 320 to 2500 nm with a 5 nm step. Can be change!
     Wl = np.arange(280, 2505, 5)  # /!\ Last value is not included in the array
 # Thickness of the substrate, in nm
@@ -24,55 +23,50 @@ def get_base_parameters():
     Sol_Spec = np.interp(Wl, Wl_Sol, Sol_Spec)
     Signal_PV = np.interp(Wl, Wl_PV, Signal_PV)  # Interpolate the signal
     Signal_Th = np.interp(Wl, Wl_Th, Signal_Th)  # Interpolate the signal
-    parameters.update({
-        'Wl':  Wl,  # Thickness of the substrate, in nm
-        'Ang': 0,  # Incidence angle on the thin layers stack, in °
-        'C': 80,  # Solar concentration. Data necessary for solar thermal application, like selective stack
-        'T_abs': 300 + 273,  # Thermal absorber temperature, in Kelvin. Data necessary for solar thermal application, like selective stack
+
+    parameters = solpoc.get_parameters(
+        seed = 3245298,
+        Wl =  Wl,  # Thickness of the substrate, in nm
+        Ang = 0,  # Incidence angle on the thin layers stack, in °
+        C = 80,  # Solar concentration. Data necessary for solar thermal application, like selective stack
+        T_abs = 300 + 273,  # Thermal absorber temperature, in Kelvin. Data necessary for solar thermal application, like selective stack
         # Air temperature, in Kelvin. Data necessary for solar thermal application, like selective stack
-        'T_air': 20 + 273,
-        'Sol_Spec': Sol_Spec,
-        'name_Sol_Spec': name_Sol_Spec,
-        'Th_range': (0, 200),  # in nm.,
-        'Th_Substrate':  1e6,  # Substrate thickness, in nm
-        'Signal_PV': Signal_PV,
-        'Signal_Th': Signal_Th,
-        'Mat_Stack': Mat_Stack,
+        T_air = 20 + 273,
+        Sol_Spec = Sol_Spec,
+        name_Sol_Spec = name_Sol_Spec,
+        Th_range = (0, 200),  # in nm.,
+        Th_Substrate =  1e6,  # Substrate thickness, in nm
+        Signal_PV = Signal_PV,
+        Signal_Th = Signal_Th,
+        Mat_Stack = Mat_Stack,
         # Cuting Wavelenght. Data necessary for low-e, RTR or PV_CSP evaluates functions
-        'Lambda_cut_1': 800,  # nm
-        'Lambda_cut_2': 1000,  # nm
+        Lambda_cut_1 = 800,  # nm
+        Lambda_cut_2 = 1000,  # nm
         # Range of refractive index (lower bound and upper bound), for the optimisation process
-        'n_range': (1.3, 3.0),
-        'n_Stack': n_Stack,
-        'k_Stack': k_Stack,
-    })
+        n_range = (1.3, 3.0),
+        n_Stack = n_Stack,
+        k_Stack = k_Stack,
+    )
 
     return parameters
 
 
 def get_DEvol_parameters():
-    parameters = {}
-    # Hyperparameters for optimisation methods
-    # Choice of optimisation method
-    algo = solpoc.DEvol  # Name of the optimization method
-    # Callable. Name of the selection method : selection_max or selection_min
-    selection = solpoc.selection_max
-    evaluate = solpoc.evaluate_T_s  # Callable. Name of the cost function
-    parameters.update({
-        'pop_size': 30,  # number of individual per iteration / generation ,
-        'algo': algo,  # Name of the optimization method ,
-        'name_algo': algo.__name__,
-        'evaluate': evaluate,  # Callable. Name of the cost function,
+    parameters = get_base_parameters()
+    # Hyperparameters for optimisation method
+    parameters. update(dict(
+        pop_size = 30,  # number of individual per iteration / generation ,
+        algo = solpoc.DEvol,  # Name of the optimization method ,
+        evaluate = solpoc.evaluate_T_s,  # Callable. Name of the cost function,
         # Callable. Name of the selection method : selection_max or selection_min,
-        'selection': selection,
-        'name_selection': selection.__name__,
+        selection = solpoc.selection_max,
         # chance of child gene muted during the birth. /!\ This is Cr for DEvol optimization method
-        'mutation_rate': 0.5,
+        mutation_rate = 0.5,
         # If a chromosome mutates, the value change from random number include between + or - this values
-        'mutation_delta': 15,
+        mutation_delta = 15,
         # Number of generation/iteration. For DEvol is also used to calculate the budget (nb_generation * pop_size)
-        'nb_generation': 4,
-        'mutation_DE': "current_to_best",
-        'f1': 0.9, 'f2': 0.8,  # Hyperparameter for DEvol
-    })
+        budget = 4*30,
+        mutation_DE = "current_to_best",
+        f1 = 0.9, f2 = 0.8,  # Hyperparameter for DEvol
+    ))
     return parameters
